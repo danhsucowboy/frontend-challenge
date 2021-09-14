54cb721af459ca032218fd0df3922f41c7ba88b1
Dan Hsu <hsu86110@gmail.com>
2021-09-14 12:00:16 +0800

    update: modified fibonacci function

--- fibonacci-number.js
+++ fibonacci-number.js
@@ -1,19 +1,11 @@
 
-const fibonacci = (position) => {
-    let a = 0, b = 1, temp = 0;
-
-    if(position === 0)
+const fibonacci = (position) => {  
+    if(position <= 0)
         return 0;
     else if(position === 1)
         return 1;
 
-    for(let i = 2; i <= position; i++){
-        temp = a+b;
-        a = b;
-        b = temp;
-    }
-
-    return temp;
+    return fibonacci(position-1) + fibonacci(position-2);
 }
 
-console.log(fibonacci(0));
+console.log(fibonacci(7));
